9cbc1bc1064141546f99ba19afd27687841abefb
divya <divya.rajwani@yahoo.com>
2018-09-19 14:05:42 +0530

    cors addition

--- app.js
+++ app.js
@@ -51,7 +51,11 @@ app.use(
 
 );
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', routes.index);
 app.get('/students', customers.list);
